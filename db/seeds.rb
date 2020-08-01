f9b0a2dc6a0f3b17f5107c206623c63439666a6d
Davide Zanetto <davide.zanetto@gmail.com>
2020-08-01 16:38:48 +0900

    patients seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,12 +12,31 @@
 # name contact specialty
 require 'faker'
 
-SPECIALTIES = w%( Gastroenterologists Pulmonologists Gynecologists Surgeon )
-
+SPECIALTIES = w%( gastroenterologists pulmonologists gynecologists surgeon )
+puts "Creating doctors"
 6.times do
   params = {}
   params[:name] = Faker::Name.name
   params[:contact] = Faker::PhoneNumber.cell_phone
   params[:specialty] = SPECIALTIES.sample
+  new_doctor = Doctor.new(params)
+  puts "Created doctor #{new_doctor.id}" if new_doctor.save
 end
+puts "Finished creating doctors"
 
+# ---------------------------------------------
+# Patients
+# name gender age severity location doctor
+GENDERS = w%( male female )
+puts "Creating patients"
+30.times do
+  params = {}
+  params[:name] = Faker::Name.name
+  params[:gender] = GENDERS.sample
+  params[:age] = rand(15..80)
+  params[:location] = "#{rand(1..7)}#{rand(0..1)}#{rand(1..9)}-0#{rand(1..8)}"
+  params[:doctor] = Doctor.all.sample
+  new_patient = Patient.new(params)
+  puts "Created patient #{new_patient.id}" if new_patient.save
+end
+puts "Finished creating patients"
