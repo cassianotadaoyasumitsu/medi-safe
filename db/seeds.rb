0c618e95b28e477192a399357fd655e24b9ef195
Davide Zanetto <davide.zanetto@gmail.com>
2020-08-01 16:59:08 +0900

    faker gem and seed clean up

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,12 +7,17 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+puts "Cleaning up start"
+Doctor.destroy_all
+Patient.destroy_all
+puts "Cleaning up done"
+
 # ---------------------------------------------
 # Doctors
 # name contact specialty
 require 'faker'
 
-SPECIALTIES = w%( gastroenterologists pulmonologists gynecologists surgeon )
+SPECIALTIES = %w( gastroenterologists pulmonologists gynecologists surgeon )
 puts "Creating doctors"
 6.times do
   params = {}
@@ -27,14 +32,15 @@ puts "Finished creating doctors"
 # ---------------------------------------------
 # Patients
 # name gender age severity location doctor
-GENDERS = w%( male female )
+GENDERS = %w( male female )
 puts "Creating patients"
 30.times do
   params = {}
   params[:name] = Faker::Name.name
   params[:gender] = GENDERS.sample
   params[:age] = rand(15..80)
-  params[:location] = "#{rand(1..7)}#{rand(0..1)}#{rand(1..9)}-0#{rand(1..8)}"
+  params[:room] = "#{rand(1..7)}#{rand(0..1)}#{rand(1..9)}"
+  params[:bed] = "0#{rand(1..8)}"
   params[:doctor] = Doctor.all.sample
   new_patient = Patient.new(params)
   puts "Created patient #{new_patient.id}" if new_patient.save
