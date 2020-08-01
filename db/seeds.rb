4142ba1bae35cf73d112303afc9aba7cdbfbdff3
Davide Zanetto <davide.zanetto@gmail.com>
2020-08-01 16:20:20 +0900

    doctor seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+# ---------------------------------------------
+# Doctors
+# name contact specialty
+require 'faker'
+
+SPECIALTIES = w%( Gastroenterologists Pulmonologists Gynecologists Surgeon )
+
+6.times do
+  params = {}
+  params[:name] = Faker::Name.name
+  params[:contact] = Faker::PhoneNumber.cell_phone
+  params[:specialty] = SPECIALTIES.sample
+end
+
