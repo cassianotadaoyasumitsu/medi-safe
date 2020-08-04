3b2a277162c2d32aca5647663416331a786f5f17
Liam Baker <liam.baker2310@gmail.com>
2020-08-04 22:47:36 +0900

    work around to show unqiue patients

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,5 +1,5 @@
 class PatientsController < ApplicationController
   def index
-    @patients = current_user.nurse_tasks.flat_map { |task| task.task_template.patient }
+    @patients = current_user.nurse_tasks.flat_map { |task| task.task_template.patient }.uniq
   end
 end
