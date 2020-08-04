8dfcf0a71dd42a917aeb17f5f2614ec27cdd9ccf
Liam Baker <liam.baker2310@gmail.com>
2020-08-04 22:31:33 +0900

    Needs modifying in the future, 95% done

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,6 +1,5 @@
 class PatientsController < ApplicationController
   def index
-    nurse_tasks = current_user.nurse_tasks
-    @patients = tasks.flat_map { |task| Patient.where(nurse_tasks: task) }
+    @patients = current_user.nurse_tasks.flat_map { |task| task.task_template.patient }
   end
 end
