ff47a57cb3039333cf9f035cbf7d4d46ba3885d6
Liam Baker <liam.baker2310@gmail.com>
2020-08-08 12:27:58 +0900

    created tabs to see current and previous tasks

--- app/controllers/patients_controller.rb
+++ app/controllers/patients_controller.rb
@@ -1,9 +1,10 @@
 class PatientsController < ApplicationController
-
   def show
     @patient = Patient.find(params[:id])
     @doctor = @patient.doctor
     @tasktemplates = TaskTemplate.where(patient: @patient)
+    @current_tasks = @patient.nurse_tasks.where(updated_at: 24.hours.ago..Time.now)
+    @yesterdays_tasks = @patient.nurse_tasks.where(updated_at: 48.hours.ago..24.hours.ago)
   end
 
   def index
