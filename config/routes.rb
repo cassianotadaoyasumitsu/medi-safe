1e24db59d4408dbe8d4d9c34110919ce7bab39e3
cassiano yasumitsu <cassiano@usuarioincomum.com>
2020-08-13 20:05:47 +0900

    user patients

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
     end
   end
   resources :patients, only: [:index, :show]
-  resources :users, only: [:index]
+  resources :users, path: :nurses, only: [:index]
 end
