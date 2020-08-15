9af4680038a8a26821b19685f7467a15e9a6a0ed
Davide Zanetto <davide.zanetto@gmail.com>
2020-08-15 11:18:41 +0900

    partial fix to js

--- app/javascript/components/progress_bar.js
+++ app/javascript/components/progress_bar.js
@@ -1,33 +1,33 @@
-var ProgressBar = require('progressbar.js')
-const value = document.getElementById('task-progress')
+function make_pie_chart() {
+  var ProgressBar = require('progressbar.js');
+  var bar = new ProgressBar.Circle('#task-progress', {
+    color: '#aaa',
+    // This has to be the same size as the maximum width to
+    // prevent clipping
+    strokeWidth: 8,
+    trailWidth: 4,
+    easing: 'bounce',
+    duration: 2400,
+    text: {
+      autoStyleContainer: false
+    },
+    from: { color: '#aaa', width: 4 },
+    to: { color: '#333', width: 8 },
+    // Set default step function for all animate calls
+    step: function(state, circle) {
+      circle.path.setAttribute('stroke', state.color);
+      circle.path.setAttribute('stroke-width', state.width);
 
+      var value = Math.round(circle.value() * 100);
+      if (value === 0) {
+        circle.setText('');
+      } else {
+        circle.setText(`${value}%`);
+      }
 
-var bar = new ProgressBar.Circle('#task-progress', {
-  color: '#aaa',
-  // This has to be the same size as the maximum width to
-  // prevent clipping
-  strokeWidth: 8,
-  trailWidth: 4,
-  easing: 'bounce',
-  duration: 2400,
-  text: {
-    autoStyleContainer: false
-  },
-  from: { color: '#aaa', width: 4 },
-  to: { color: '#333', width: 8 },
-  // Set default step function for all animate calls
-  step: function(state, circle) {
-    circle.path.setAttribute('stroke', state.color);
-    circle.path.setAttribute('stroke-width', state.width);
-
-    var value = Math.round(circle.value() * 100);
-    if (value === 0) {
-      circle.setText('');
-    } else {
-      circle.setText(`${value}%`);
     }
+  });
+  return bar;
+}
 
-  }
-});
-
-export { bar };
+export { make_pie_chart };
