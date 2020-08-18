99194dbc723f8f584a15f46d6edd65583cb5342a
Davide Zanetto <davide.zanetto@gmail.com>
2020-08-18 21:52:02 +0900

    now sort path calculates schedule

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_13_112604) do
+ActiveRecord::Schema.define(version: 2020_08_18_110733) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2020_08_13_112604) do
     t.integer "position"
     t.integer "slot"
     t.boolean "active", default: false
+    t.time "start_time"
     t.index ["helper_id"], name: "index_nurse_tasks_on_helper_id"
     t.index ["task_template_id"], name: "index_nurse_tasks_on_task_template_id"
     t.index ["user_id"], name: "index_nurse_tasks_on_user_id"
