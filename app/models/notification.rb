81b1dbed3c3aaf86bf81c0d719664e950cb5834a
Liam Baker <liam.baker2310@gmail.com>
2020-08-25 10:34:39 +0900

    added comments to some notification code for readability

--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,6 +1,7 @@
 class Notification < ApplicationRecord
   belongs_to :recipient, class_name: 'User'
   belongs_to :actor, class_name: 'User'
+  # This allows notifiable to accept multiple different classes
   belongs_to :notifiable, polymorphic: true
 
   scope :unread, -> { where(read_at: nil) }
